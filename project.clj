7edaa61776e3cb5287262ca4b2398a3894b8fed8
Brian Murphy-Dye <brian@murphydye.com>
2016-06-02 06:04:25 -0600

    Update git url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.murphydye/utils "0.1.0-SNAPSHOT"
   :description "Random, unorganized utility functions"
-  :url "http://example.com/FIXME"
+  :url "https://github.com/brianmd/utils.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]])
