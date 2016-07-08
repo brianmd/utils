4ca789018669b593f2a655571d45a8ba8682ef61
Brian Murphy-Dye <brian@murphydye.com>
2016-07-07 19:53:39 -0600

    Fix url.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject com.murphydye/utils "0.1.1"
   :description "Random, unorganized utility functions"
-  :url "https://github.com/brianmd/utils.git"
+  :url "https://github.com/brianmd/utils"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
