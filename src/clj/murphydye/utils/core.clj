15ed31cc4221d967fd066776a1ed1f755c7aea74
Brian Murphy-Dye <brian@murphydye.com>
2016-11-10 14:29:05 -0700

    Add deep-merge.

--- src/clj/murphydye/utils/core.clj
+++ src/clj/murphydye/utils/core.clj
@@ -610,5 +610,21 @@
  (reset-pp)
  )
 
+
+
+
+
+(defn deep-merge
+  "Recursively merges maps. If keys are not maps, the last value wins."
+  [& vals]
+  (if (every? map? vals)
+      (apply merge-with deep-merge vals)
+      (last vals)))
+
+
+
+
+
+
 (println "done loading com.murphydye.utils.core")
 (println "-----------------\n\n")
